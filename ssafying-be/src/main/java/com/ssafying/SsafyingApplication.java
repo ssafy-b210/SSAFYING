c65769183af3353be333ecfeeed74c0e1c35db20
2_yewon <2_yewon@naver.com>
2024-01-16 16:11:05 +0900

    init: create BE project

---
+++ ssafying-be/src/main/java/com/ssafying/SsafyingApplication.java
@@ -0,0 +1,13 @@
+package com.ssafying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafyingApplication.class, args);
+	}
+
+}
