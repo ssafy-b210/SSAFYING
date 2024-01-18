149bc5d1f3bf0d0e2ac4d51fe00273f6213cd0aa
이수민 <judy3504@naver.com>
2024-01-18 17:43:50 +0900

    feat: 머지상태확인

--- ssafying-fe/src/pages/User/UserLogin.tsx
+++ ssafying-fe/src/pages/User/UserLogin.tsx
@@ -1,16 +1,11 @@
-import logo from "../../assets/img/logo1.svg";
+import LoginForm from "../../components/User/LoginForm";
+
 function UserLogin() {
   return (
     <div>
-      <img src={logo} alt="ssafying"></img>
-      <h4>우리는 여전히 싸피중</h4>
-      <form>
-        <input type="email" placeholder="이메일을 입력하세요"></input> <br />
-        <input type="password" placeholder="비밀번호를 입력하세요"></input>
-        <input type="checkbox"></input>
-        <input type="submit" value="로그인하기"></input>
-      </form>
+      <LoginForm />
     </div>
   );
 }
+
 export default UserLogin;
