091bb853021e47d62994c9261b125ece0202652f
이수민 <judy3504@naver.com>
2024-01-18 10:22:09 +0900

    init: 타입스크립트로 변경

---
+++ ssafying-fe/src/pages/User/UserLogin.tsx
@@ -0,0 +1,15 @@
+function UserLogin() {
+  return (
+    <div>
+      <h1>ssafying</h1>
+      <h4>우리는 여전히 싸피중</h4>
+      <form>
+        <input type="email" placeholder="이메일을 입력하세요"></input> <br />
+        <input type="password" placeholder="비밀번호를 입력하세요"></input>
+        <input type="checkbox"></input>
+        <input type="submit" value="로그인하기"></input>
+      </form>
+    </div>
+  );
+}
+export default UserLogin;
---
+++ ssafying-fe/src/pages/User/UserSignup.tsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <form></form>
+    </div>
+  );
+}
+export default About;
