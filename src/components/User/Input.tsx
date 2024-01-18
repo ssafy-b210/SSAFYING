98cdda4cd1c692bd67221c3e49e8c1214e0184e8
djsumin <136299367+djsumin@users.noreply.github.com>
2024-01-19 00:10:12 +0900

    [design]회원가입 헤더 수정중

--- src/components/User/Input.tsx
+++
@@ -1,7 +0,0 @@
-import styled, { css } from "styled-components";
-import { ChangeEvent, MouseEvent, useState } from "react";
-
-interface InputProps {
-  isSmallInnerText: boolean;
-  isFocus: boolean | undefined;
-}
