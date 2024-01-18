2d69cda8c4de8d50e2eac6f0b6b548016b0e74d5
이수민 <judy3504@naver.com>
2024-01-18 15:52:33 +0900

    feat: 컴포넌트와 페이지 분리

--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -1,51 +1,11 @@
-// eslint-disable-next-line
-import styled from "styled-components";
-
-import googleIcon from "../../assets/SocialLoginIcons/googleIcon.png";
-import githubIcon from "../../assets/SocialLoginIcons/githubIcon.png";
-import kakaoIcon from "../../assets/SocialLoginIcons/kakaoIcon.png";
-
-const SocialLoginBox = styled.div`
-  display: flex;
-  margin-top: 10px;
-`;
-
-const SocialLogin = styled.div`
-  width: 30px;
-  height: 30px;
-  img {
-    width: 100%;
-    height: 100%;
-  }
-`;
+import LoginForm from "../../components/User/LoginForm";
 
 function UserLogin() {
   return (
     <div>
-      <h1>ssafying</h1>
-      <h4>우리는 여전히 싸피중</h4>
-      <form>
-        <input type="email" placeholder="이메일을 입력하세요"></input> <br />
-        <input type="password" placeholder="비밀번호를 입력하세요"></input>
-        <input type="checkbox"></input>
-        <br />
-        <input type="submit" value="로그인하기"></input>
-        <SocialLoginBox>
-          <SocialLogin>
-            <img src={googleIcon} alt="Google Icon" />
-          </SocialLogin>
-          <SocialLogin>
-            <img src={githubIcon} alt="Github Icon" />
-          </SocialLogin>
-          <SocialLogin>
-            <img src={kakaoIcon} alt="Kakao Icon" />
-          </SocialLogin>
-        </SocialLoginBox>
-      </form>
-      <hr />
-      <h4>등록된 계정이 없다면</h4>
-      <button>회원가입</button>
+      <LoginForm />
     </div>
   );
 }
+
 export default UserLogin;
