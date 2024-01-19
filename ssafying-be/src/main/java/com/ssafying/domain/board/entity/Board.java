cb18ac1b4c45031eaca9548c7979e812f063bfaa
Hgstudio <tndms0308@naver.com>
2024-01-19 15:42:13 +0900

    feat : 자유게시판 entity 생성

---
+++ ssafying-be/src/main/java/com/ssafying/domain/board/entity/Board.java
@@ -0,0 +1,37 @@
+package com.ssafying.domain.board.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board") //자유게시판
+@Getter
+public class Board {
+
+    @Id @GeneratedValue
+    @Column(name="board_id")
+    private int id; //자유게시판 게시글 id
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user; //게시물 등록 회원
+
+    @Enumerated(EnumType.STRING)
+    private CategoryStatus category; //카테고리
+
+    private String title; //제목
+
+    private String content; //내용
+
+    @Column(name = "is_anonymous")
+    private boolean isAnonymous; //익명여부
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt; //생성일자
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt; //수정일자
+
+}
---
+++ ssafying-be/src/main/java/com/ssafying/domain/board/entity/CategoryStatus.java
@@ -0,0 +1,6 @@
+package com.ssafying.domain.board.entity;
+
+public enum CategoryStatus {
+    FREEDOM, EMPLOYMENT, INFO, DEVELOPMENT, TIP, LIVING, PROMOTION
+    //자유, 취업, 정보, 개발, 싸피꿀팁, 생활, 홍보
+}
