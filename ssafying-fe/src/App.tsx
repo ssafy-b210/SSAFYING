357f5c7aacc5cce4ee69cc81466036138f1b9ae9
김혜진 <hyejin9203@naver.com>
2024-01-20 16:56:18 +0900

    Revert "Merge branch 'feature-fe/feedmain' into 'master'"
    This reverts commit 63bd7085de3bec3358f6e1ef372b63b6e43794ee, reversing
    changes made to 3e6402635ce56e1c22baa97aadfbf89e573e079f.

--- ssafying-fe/src/App.tsx
+++ ssafying-fe/src/App.tsx
@@ -1,28 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import UserLogin from "./pages/User/UserLogin";
 import UserSignup from "./pages/User/UserSignup";
-import ProfileMain from "./pages/Profile/ProfileMain";
-import styled from "styled-components";
 
 function App() {
   return (
-    <Wrapper>
-      <Routes>
-        <Route path="/" element={<UserLogin />} />
-        <Route path="/signup" element={<UserSignup />} />
-        <Route path="/profile" element={<ProfileMain />} />
-      </Routes>
-    </Wrapper>
+    <Routes>
+      <Route path="/" element={<UserLogin />} />
+      <Route path="/signup" element={<UserSignup />} />
+    </Routes>
   );
 }
 
 export default App;
-
-const Wrapper = styled.div`
-  max-width: 560px;
-  height: 100vh;
-  margin: 0 auto;
-  @supports (-webkit-touch-callout: none) {
-    height: -webkit-fill-available;
-  }
-`;
