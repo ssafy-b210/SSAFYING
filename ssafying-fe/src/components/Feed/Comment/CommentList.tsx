1c225ac2b6ab5a2c2234a7553c272bd8ffc2b21c
김혜진 <hyejin9203@naver.com>
2024-01-22 23:14:42 +0900

    design: 댓글 리스트

---
+++ ssafying-fe/src/components/Feed/Comment/CommentList.tsx
@@ -0,0 +1,9 @@
+import CommentItem from "./CommentItem";
+function CommentList() {
+  return (
+    <div>
+      <CommentItem commentId="aeong" userId="aeong" content="ㅋㅋㅋㅋㅋ"/>
+    </div>
+  );
+}
+export default CommentList;
