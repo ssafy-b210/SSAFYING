f9fb1083ec5c402843709d0cd224a76e895b7cd5
임지은 <ljieun01@naver.com>
2024-01-22 10:35:00 +0900

    design: 스크랩 탭, 버튼 UI 추가

---
+++ ssafying-fe/src/components/Profile/Scrap/ScrapContent.tsx
@@ -0,0 +1,15 @@
+import HashtagList from "./HashtagList";
+import MyFeedList from "./MyFeedList";
+import ScrapTap from "./ScrapTap";
+
+function ScrapContent() {
+  return (
+    <div>
+      <ScrapTap />
+      <HashtagList />
+      <MyFeedList />
+    </div>
+  );
+}
+
+export default ScrapContent;
