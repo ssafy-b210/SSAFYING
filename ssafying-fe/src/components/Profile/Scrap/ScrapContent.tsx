427d94c562d42d0b0e5ef6a097fe3c009d210394
임지은 <ljieun01@naver.com>
2024-01-22 12:08:55 +0900

    design: 스크랩 분류 버튼 컴포넌트 추가

--- ssafying-fe/src/components/Profile/Scrap/ScrapContent.tsx
+++ ssafying-fe/src/components/Profile/Scrap/ScrapContent.tsx
@@ -1,9 +1,11 @@
 import HashtagList from "./HashtagList";
 import MyFeedList from "./MyFeedList";
+import ScrapSortTap from "./ScrapSortTab";
 
 function ScrapContent() {
   return (
     <div>
+      <ScrapSortTap />
       <HashtagList />
       <MyFeedList />
     </div>
