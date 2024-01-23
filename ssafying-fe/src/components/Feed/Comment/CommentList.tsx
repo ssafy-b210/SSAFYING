95be8672b384c778113b60c8a849b2a4096818d5
DESKTOP-LL7FDJ6\SSAFY <hyejin9203@naver.com>
2024-01-23 17:55:14 +0900

    design: 답글 작성시 highlight

--- ssafying-fe/src/components/Feed/Comment/CommentList.tsx
+++ ssafying-fe/src/components/Feed/Comment/CommentList.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import CommentItem from "./CommentItem";
+
 function CommentList() {
+  const [isHighlighted, setIsHighlighted] = useState(false);
+
   return (
     <div>
-      <CommentItem commentId="aeong" userId="aeong" content="ㅋㅋㅋㅋㅋ" />
-      <CommentItem commentId="yes" userId="aeong" content="ㅋㅋㅋㅋㅋ" />
+      <CommentItem
+        commentId="aeong"
+        userId="aeong"
+        content="ㅋㅋㅋㅋㅋ"
+        isHighlighted={false}
+      />
+      <CommentItem
+        commentId="yes"
+        userId="aeong"
+        content="ㅋㅋㅋㅋㅋ"
+        isHighlighted={false}
+      />
+      <CommentItem
+        commentId="yes.hh"
+        userId="aeong"
+        content="안녕ㅎㅎ"
+        isHighlighted={false}
+      />
     </div>
   );
 }
--- ssafying-fe/src/components/Feed/Comment/CommentWrite.tsx
+++ ssafying-fe/src/components/Feed/Comment/CommentWrite.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import userImage from "../../../assets/img/testImg/user.svg";
 import deleteBtn from "../../../assets/img/imgBtn/deleteBtn.svg";
@@ -9,32 +9,21 @@ interface commentProps {
   commentId: string;
   userId: string;
   content: string;
-  isHighlighted: boolean;
 }
 
-function CommentItem({
-  commentId,
-  userId,
-  content,
-  isHighlighted,
-}: commentProps) {
+function CommentItem({ commentId, userId, content }: commentProps) {
   function clickDeleteBtn() {
     console.log("delete comment");
   }
 
-  function clickRecommentBtn() {
-    // Toggle the highlight state
-    setIsHighlighted(!isHighlighted);
-  }
-
   return (
-    <UserWrapper isHighlighted={isHighlighted}>
+    <UserWrapper>
       <Comment>
         <RoundImg src={userImage} size="40px" />
         <div>
           <UserId>{commentId}</UserId>
           <Content>{content}</Content>
-          <TextBtn onClick={clickRecommentBtn}>답글달기</TextBtn>
+          <TextBtn>답글달기</TextBtn>
         </div>
       </Comment>
       {commentId === userId && (
@@ -48,19 +37,16 @@ function CommentItem({
 
 export default CommentItem;
 
-const UserWrapper = styled.div<{ isHighlighted: boolean }>`
+const UserWrapper = styled.div`
   padding: 5px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: ${(props) =>
-    props.isHighlighted ? "lightyellow" : "transparent"};
 `;
 
 const UserId = styled.div`
-  margin-left: 5px;
+  margin-left: 5px; // Adjust the margin as needed
   font-weight: bold;
-  line-height: 1.1;
 `;
 
 const Comment = styled.div`
@@ -79,5 +65,4 @@ const TextBtn = styled.div`
   font-size: 9px;
   font-weight: bold;
   color: gray;
-  cursor: pointer; // Add this to show it's clickable
 `;
