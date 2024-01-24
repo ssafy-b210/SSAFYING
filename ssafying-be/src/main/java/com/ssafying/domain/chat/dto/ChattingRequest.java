f9a71921937aebf623b20b7820a2d581305f2b05
hyunseok <nhs503601@gmail.com>
2024-01-24 09:17:30 +0900

    feat: ChattingRequest Dto 추가

--- ssafying-be/src/main/java/com/ssafying/domain/chat/dto/ChattingRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/chat/dto/ChattingRequest.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class ChattingRequest {
-    private int chatRoomId; // 유저 id
-    private int userId;
+    private int chatRoomId; // 채팅방 id
+    private int userId; // 유저 id
     private String message; // 채팅메시지
 }
