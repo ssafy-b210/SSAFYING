361c36a61bfe01943686a9a755eb9717a0e215ff
이수민 <judy3504@naver.com>
2024-01-24 14:33:37 +0900

    design: 사람구해요 목록 퍼블리싱 완료

--- ssafying-fe/src/components/All/Crew/CrewList/CrewCardList.tsx
+++ ssafying-fe/src/components/All/Crew/CrewList/CrewCardList.tsx
@@ -1,8 +1,41 @@
 import styled from "styled-components";
 import React from "react";
 
+import isRecruiting from "../../../../assets/img/imgBtn/isRecruiting.svg";
+import isNotRecruiting from "../../../../assets/img/imgBtn/isNotRecruiting.svg";
+
 function CrewCardList() {
-  const [cardInfo, setCardInfo] = React.useState<CrewCardList[]>([]);
+  const [cardInfo, setCardInfo] = React.useState<CrewCardList[]>([
+    { title: "대전캠 스터디 구해요", nickname: "su00", isRecruiting: true },
+    {
+      title: "프론트 스터디 같이하실분",
+      nickname: "yes.hs",
+      isRecruiting: false,
+    },
+  ]);
+
+  type CardsProps = {
+    info: CrewCardList;
+  };
+
+  const Cards: React.FC<CardsProps> = ({ info }) => (
+    <StyledCards>
+      <h2 className="title">{info.title}</h2>
+      <hr></hr>
+      <div className="small-container">
+        <p className="nickname">{info.nickname}</p>
+        {info.isRecruiting ? (
+          <img className="isRecruiting" src={isRecruiting} alt="Recruiting" />
+        ) : (
+          <img
+            className="isRecruiting"
+            src={isNotRecruiting}
+            alt="Not Recruiting"
+          />
+        )}
+      </div>
+    </StyledCards>
+  );
 
   return (
     <CrewCardContainer>
@@ -27,22 +60,24 @@ const CrewCardContainer = styled.div`
   justify-content: center;
 `;
 
-type CardsProps = {
-  info: CrewCardList;
-};
-
-const Cards: React.FC<CardsProps> = ({ info }) => (
-  <StyledCards>
-    <p>{info.title}</p>
-    <p>{info.nickname}</p>
-    <p>{info.isRecruiting.toString()}</p>
-  </StyledCards>
-);
-
 const StyledCards = styled.div`
   width: 200px;
   height: 200px;
   border: 3px solid #c4c4c4;
   border-radius: 20px;
   margin: 20px;
+  display: flex;
+  flex-direction: column;
+  hr {
+    width: 80%;
+  }
+  .title {
+    text-align: left;
+    margin-left: 15px;
+  }
+  .small-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
 `;
