c35612b2b8260a5560dc0c7a65db93639825e2e2
임지은 <ljieun01@naver.com>
2024-01-25 16:48:32 +0900

    design: 팔로우, 팔로잉 리스트 페이지 추가
    Change-Id: I0255bf0d2b976f43ea2037bd40abcbae9f910e7e

--- ssafying-fe/src/pages/Profile/FollowingList.tsx
+++ ssafying-fe/src/pages/Profile/FollowingList.tsx
@@ -1,9 +1,45 @@
+import styled from "styled-components";
+import UserItemList from "../../components/Feed/Search/UserItemList";
+import FollowHeader from "../../components/Profile/Follow/FollowHeader";
+
 function FollowingList() {
   return (
     <div>
-      <div>Following List</div>
+      <FollowHeader text="내가 팔로우하는 친구" />
+      <SearchBar>
+        <input type="text" placeholder="검색어를 입력해주세요." />
+      </SearchBar>
+      <UserItemList
+        userList={[
+          {
+            userId: "aeong",
+            userImage:
+              "https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg",
+          },
+          {
+            userId: "aeong2",
+            userImage:
+              "https://image.utoimage.com/preview/cp872722/2018/06/201806010732_206.jpg",
+          },
+        ]}
+      />
     </div>
   );
 }
 
 export default FollowingList;
+
+const SearchBar = styled.div`
+  position: relative;
+  display: flex;
+  margin: 0 10px;
+
+  input {
+    width: 100%;
+    background-color: lightgray;
+    border-radius: 10px;
+    padding: 10px;
+    border: none;
+    margin: 0 5px;
+  }
+`;
