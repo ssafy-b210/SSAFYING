05b7e69e8c65499c2b79273041231963c8ba2997
tndms0308 <tndms0308@naver.com>
2024-01-29 01:06:00 +0900

    feat : Board Entity에 SuperClass 추가

--- ssafying-be/src/main/java/com/ssafying/domain/board/entity/Board.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/entity/Board.java
@@ -1,6 +1,7 @@
 package com.ssafying.domain.board.entity;
 
 import com.ssafying.domain.user.entity.User;
+import com.ssafying.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "board") //자유게시판
 @Getter
-
-public class Board {
+public class Board extends BaseTimeEntity {
 
     @Id @GeneratedValue
     @Column(name="board_id")
@@ -21,19 +21,37 @@ public class Board {
     @JoinColumn(name = "user_id")
     private User user; //게시물 등록 회원
 
-    @Enumerated(EnumType.STRING)
-    private CategoryStatus category; //카테고리
-
     private String title; //제목
 
     private String content; //내용
 
+    @Enumerated(EnumType.STRING)
+    private CategoryStatus category; //카테고리
+
     @Column(name = "is_anonymous")
     private boolean isAnonymous; //익명여부
 
-    @Column(name = "created_at")
-    private LocalDateTime createdAt; //생성일자
+    //생성일자와 수정일자는 BaseTimeEntity 에 있음
 
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt; //수정일자
+
+    //그냥 생성자를 사용하면 어떤 경우에 사용되는지 알 수 없음.
+    //이렇게 특정한 경우 사용되는 함수를 정의해주면 더 명확하게 의도를 알 수 있음.
+    public static Board createBoard(
+        String title,
+        String content,
+        String category,
+        boolean isAnonymous,
+        User user
+    ){
+        Board board = new Board();
+
+        //파라미터로 넘어온 값 세팅
+        board.title = title;
+        board.content = content;
+        board.category = CategoryStatus.valueOf(category);
+        board.isAnonymous = isAnonymous;
+        board.user = user;
+
+        return board;
+    }
 }
