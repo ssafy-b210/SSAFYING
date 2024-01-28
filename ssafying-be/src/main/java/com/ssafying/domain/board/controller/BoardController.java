eed5cfd9b7291b92c3a1a0e17c800c275ab38bed
tndms0308 <tndms0308@naver.com>
2024-01-29 01:44:11 +0900

    feat : BoardController 게시글 작성 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/controller/BoardController.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/controller/BoardController.java
@@ -1,17 +1,15 @@
 package com.ssafying.domain.board.controller;
 
+import com.ssafying.domain.board.dto.request.CreateBoardRequest;
 import com.ssafying.domain.board.service.BoardService;
 import com.ssafying.global.result.ResultResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 //@Api
 @RestController
@@ -24,11 +22,18 @@ public class BoardController {
     /**
      * 5.1 게시판 게시글 작성
      */
-//    @PostMapping("board/article")
-//    public ResponseEntity<ResultResponse> createArticle() {
-//
-//
-//    }
+    @PostMapping("board/article")
+    public ResponseEntity<ResultResponse> createBoard(@RequestBody @Valid CreateBoardRequest request) {
+
+        boardService.createBoard(request);
+
+
+        return null;
+    }
+
+    /**
+     * 5.2 게시판
+     */
 
 
 
