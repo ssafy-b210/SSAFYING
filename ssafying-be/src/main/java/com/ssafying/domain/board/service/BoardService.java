e6577929599bbb54f02b97abdff65e2cf3cb60fe
tndms0308 <tndms0308@naver.com>
2024-01-30 01:15:06 +0900

    feat : BoardService 게시글 스크랩 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/service/BoardService.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/service/BoardService.java
@@ -1,10 +1,13 @@
 package com.ssafying.domain.board.service;
 
 import com.ssafying.domain.board.dto.request.CreateBoardRequest;
+import com.ssafying.domain.board.dto.request.ScrapBoardRequest;
 import com.ssafying.domain.board.entity.Board;
+import com.ssafying.domain.board.entity.BoardScrap;
 import com.ssafying.domain.board.repository.jdbc.BoardRepository;
+import com.ssafying.domain.board.repository.jdbc.BoardScarpRepository;
 import com.ssafying.domain.user.entity.User;
-import com.ssafying.domain.user.repository.jdbc.UserRepository;
+import com.ssafying.domain.user.repository.jdbc.UserRepositorySDJ;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +20,8 @@ import java.util.List;
 public class BoardService {
 
     private final BoardRepository boardRepository;
-    private final UserRepository userRepository;
+    private final UserRepositorySDJ userRepository;
+    private final BoardScarpRepository boardScarpRepository;
 
     /**
      * 5.1 게시판 게시글 작성
@@ -50,6 +54,11 @@ public class BoardService {
     /**
      * 5.2 게시판 게시글 조회
      */
+
+
+    /**
+     * 5.3 게시판 게시글 스크랩
+     */
     public List<Board> findBoard(int pageNo, String searchCategory, String searchWord) {
 
         //searchCategory 검사하기
@@ -57,9 +66,25 @@ public class BoardService {
 
 //        boardRepository.
 
-
-
         return null;
     }
 
+    public int scrapBoard(ScrapBoardRequest request) {
+
+        //request로 넘어온 userId가 존재하는지 확인
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("유저가 없습니다."));
+
+        //request 로 넘어온 boardId가 존재하는지 확인
+        Board board = boardRepository.findById(request.getBoardId())
+                .orElseThrow(() -> new RuntimeException("스크랩하려는 게시글이 존재하지 않습니다."));
+
+        //유저와 스크랩하려는 게시글이 존재한다면 BoardScrap 생성
+        BoardScrap boardScrap = BoardScrap.createBoardScrap(user, board);
+
+        //디비에 저장
+        BoardScrap save = boardScarpRepository.save(boardScrap);
+
+        return save.getId();
+    }
 }
