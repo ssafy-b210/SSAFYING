ad0b5f3c224e28bb4f1effb68321e1b589b46d73
tndms0308 <tndms0308@naver.com>
2024-01-30 01:18:04 +0900

    feat : BoardServiceTest 게시글스크랩 구현

--- ssafying-be/src/test/java/com/ssafying/domain/board/service/BoardServiceTest.java
+++ ssafying-be/src/test/java/com/ssafying/domain/board/service/BoardServiceTest.java
@@ -1,20 +1,22 @@
 package com.ssafying.domain.board.service;
 
 import com.ssafying.domain.board.dto.request.CreateBoardRequest;
+import com.ssafying.domain.board.dto.request.ScrapBoardRequest;
 import com.ssafying.domain.board.entity.Board;
+import com.ssafying.domain.board.entity.BoardScrap;
 import com.ssafying.domain.board.entity.CategoryStatus;
 import com.ssafying.domain.board.repository.jdbc.BoardRepository;
+import com.ssafying.domain.board.repository.jdbc.BoardScarpRepository;
 import com.ssafying.domain.user.entity.User;
 import com.ssafying.domain.user.repository.jdbc.UserRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +24,9 @@ public class BoardServiceTest {
 
     @Autowired BoardService boardService;
     @Autowired BoardRepository boardRepository;
+    @Autowired UserRepositorySDJ userRepositorySDJ;
+    @Autowired
+    BoardScarpRepository boardScarpRepository;
     @Autowired
     UserRepository userRepository;
 
@@ -50,5 +55,41 @@ public class BoardServiceTest {
         Board board = find.get();
         assertThat(board.getTitle()).isEqualTo("title");
         assertThat(board.getContent()).isEqualTo("content");
+    }
+
+    @Test
+    void 게시글스크랩() throws Exception {
+        // given
+
+        //유저 생성
+        User user = new User();
+        user.setName("순");
+        userRepositorySDJ.save(user);
+
+        //게시글 생성
+        Board board = Board.createBoard(
+                "title",
+                "content22",
+                CategoryStatus.FREEDOM,
+                false,
+                user
+        );
+        boardRepository.save(board);
+
+        //메서드 req 생성
+        ScrapBoardRequest req = ScrapBoardRequest.builder()
+                .userId(user.getId())
+                .boardId(board.getId())
+                .build();
+
+        // when
+        //메서드 호출
+        int boardScrapId = boardService.scrapBoard(req);
+        Optional<BoardScrap> find = boardScarpRepository.findById(boardScrapId);
+
+        // then
+        BoardScrap boardScrap = find.get();
+        assertThat(boardScrap.getBoard().getContent()).isEqualTo("content22");
+        assertThat(boardScrap.getUser().getName()).isEqualTo("순");
     }
 }
