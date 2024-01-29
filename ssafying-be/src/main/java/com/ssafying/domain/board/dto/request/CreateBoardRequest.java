8bfce91975d676cc397d3d2dd41c1e5c35e244eb
Hgstudio <tndms0308@naver.com>
2024-01-29 17:55:41 +0900

    feat : CreateBoardRequest category의 자료형 수정

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/CreateBoardRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/CreateBoardRequest.java
@@ -1,5 +1,6 @@
 package com.ssafying.domain.board.dto.request;
 
+import com.ssafying.domain.board.entity.CategoryStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -12,14 +13,14 @@ public class CreateBoardRequest {
     @NotNull(message = "유저 PK는 필수입니다.")
     int userId;
 
-    @NotNull(message = "제목은 필수입니다.")
+//    @NotNull(message = "제목은 필수입니다.")
     String title;
 
     @NotNull(message = "게시글 내용은 필수입니다.")
     String content;
 
     @NotNull(message = "는 필수입니다.")
-    String category;
+    CategoryStatus category;
 
     @NotNull(message = "익명 여부는 필수입니다.")
     boolean isAnonymous;
