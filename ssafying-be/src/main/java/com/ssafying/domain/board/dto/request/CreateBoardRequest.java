690acef41e185eefcdc93b0a89e92263135bd882
tndms0308 <tndms0308@naver.com>
2024-01-30 01:19:00 +0900

    feat : CreateBoardRequest 추가

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/CreateBoardRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/CreateBoardRequest.java
@@ -2,7 +2,8 @@ package com.ssafying.domain.board.dto.request;
 
 import com.ssafying.domain.board.entity.CategoryStatus;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
 
 @Getter
 //@AllArgsConstructor
@@ -13,13 +14,13 @@ public class CreateBoardRequest {
     @NotNull(message = "유저 PK는 필수입니다.")
     int userId;
 
-//    @NotNull(message = "제목은 필수입니다.")
+    @NotNull(message = "제목은 필수입니다.")
     String title;
 
     @NotNull(message = "게시글 내용은 필수입니다.")
     String content;
 
-    @NotNull(message = "는 필수입니다.")
+    @NotNull(message = "category 는 필수입니다.")
     CategoryStatus category;
 
     @NotNull(message = "익명 여부는 필수입니다.")
