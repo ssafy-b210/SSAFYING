12936e6a5206cbb841850b105eedeaf1f37848e5
DESKTOP-LL7FDJ6\SSAFY <hyejin9203@naver.com>
2024-01-30 10:35:12 +0900

    chore: be 삭제
    Change-Id: I878780b8ee2097e42bbab1ab4d735b6db107f1fd

--- ssafying-be/src/main/java/com/ssafying/domain/crew/entity/Category.java
+++
@@ -1,7 +0,0 @@
-package com.ssafying.domain.crew.entity;
-
-public enum Category {
-    STUDY, CHALLENGE, SOCIETY, ACTIVITY, FLASHMOB, PROJECT, ETC;
-
-    //스터디, 챌린지, 동호회, 액티비티, 번개모임, 공모전/프로젝트, 기타
-}
