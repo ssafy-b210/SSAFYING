a501e3d34f9a47fe5a885b28fd251e6c124b0dd6
hyunseok <nhs503601@gmail.com>
2024-01-31 21:40:48 +0900

    chore : 빠진 어노테이션 추가 & 피드 Service 생성
    Change-Id: Ia8c3204c2c98e15e7ab5c52888679f5d5abef464

--- ssafying-be/src/main/java/com/ssafying/domain/feed/repository/FeedRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/feed/repository/FeedRepository.java
@@ -3,6 +3,8 @@ package com.ssafying.domain.feed.repository;
 
 import com.ssafying.domain.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeedRepository extends JpaRepository<Feed, Integer> {
 }
---
+++ ssafying-be/src/main/java/com/ssafying/domain/feed/service/FeedService.java
@@ -0,0 +1,10 @@
+package com.ssafying.domain.feed.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedService {
+
+
+
+}
