c959a3da912c1061499942e80ac14b5719b0f58d
DESKTOP-LL7FDJ6\SSAFY <hyejin9203@naver.com>
2024-02-01 17:18:45 +0900

    feat: 대나무숲 모달 추가
    Change-Id: I3003a768934b885112f4d73a0088f82ad8387d30

---
+++ ssafying-fe/src/components/BambooForest/BambooItemModal.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+interface bambooProps {
+  children: React.ReactNode;
+  btnTxt: string;
+}
+
+function BambooItemModal({ btnTxt, children }: bambooProps) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        {btnTxt}
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {children}
+      </Dialog>
+    </React.Fragment>
+  );
+}
+
+export default BambooItemModal;
