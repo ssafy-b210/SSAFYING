18656fd1db114e9c340163697e625b4677962b0a
Hgstudio <tndms0308@naver.com>
2024-02-02 00:15:39 +0900

    feat : ResultResponse 객체 구현

--- ssafying-be/src/main/java/com/ssafying/global/result/ResultResponse.java
+++ ssafying-be/src/main/java/com/ssafying/global/result/ResultResponse.java
@@ -1,10 +1,33 @@
 package com.ssafying.global.result;
 
-public class ResultResponse {
-    private int status; //Http 상태 코드
-    private String message; //응답 메세지
-    private Object data; //응답 데이터
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
 
+@Data
+@AllArgsConstructor
+@Builder
+public class ResultResponse<T> {
+    private HttpStatus statusCode;
+    private String resultMsg;
+    private T resultData;
 
+    public ResultResponse(final HttpStatus statusCode, final String resultMsg) {
+        this.statusCode = statusCode;
+        this.resultMsg = resultMsg;
+        this.resultData = null;
+    }
 
+    public static <T> ResultResponse<T> res(final HttpStatus statusCode, final String resultMsg) {
+        return res(statusCode, resultMsg, null);
+    }
+
+    public static <T> ResultResponse<T> res(final HttpStatus statusCode, final String resultMsg, final T t) {
+        return ResultResponse.<T>builder()
+                .resultData(t)
+                .statusCode(statusCode)
+                .resultMsg(resultMsg)
+                .build();
+    }
 }
