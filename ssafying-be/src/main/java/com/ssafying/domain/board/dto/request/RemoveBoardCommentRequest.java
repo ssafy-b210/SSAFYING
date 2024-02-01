04d944cef9380d3a9b74dd9e2f5c6d50de9d2fa3
Hgstudio <tndms0308@naver.com>
2024-02-02 00:44:28 +0900

    feat : JPA 더미데이터 생성을 위한 어노테이션 설정

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/RemoveBoardCommentRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/RemoveBoardCommentRequest.java
@@ -1,4 +1,11 @@
 package com.ssafying.domain.board.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
+
+//@AllArgsConstructor
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
 public class RemoveBoardCommentRequest {
 }
