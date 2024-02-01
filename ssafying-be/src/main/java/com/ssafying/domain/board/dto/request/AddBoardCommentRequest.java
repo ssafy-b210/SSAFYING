27f335ea5ff34a32a5c3cd385e4497d08d6c2cba
Hgstudio <tndms0308@naver.com>
2024-02-01 17:53:07 +0900

    feat : AddBoardCommentRequest 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/AddBoardCommentRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/AddBoardCommentRequest.java
@@ -1,4 +1,22 @@
 package com.ssafying.domain.board.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class AddBoardCommentRequest {
+
+//    @NotNull
+//    int userId; //댓글 작성한 유저
+
+    @NotNull
+    String content; //댓글 내용
+
+    @NotNull
+    boolean isAnonymous; //익명 여부
+
+    @NotNull
+    int parentId; //부모 댓글
 }
