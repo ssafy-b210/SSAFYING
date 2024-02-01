e4eceaeca2a446d55a05c4bdf391036288f1c03c
임지은 <ljieun01@naver.com>
2024-02-01 13:41:36 +0900

    design: 셔틀버스 선택 페이지 작성
    Change-Id: Ib30fe7cb527b9fd6f46ef1a2ba9918c67f79d5dc

--- ssafying-fe/src/pages/Now/BusRealTime/BusRealTimeSelect.tsx
+++ ssafying-fe/src/pages/Now/BusRealTime/BusRealTimeSelect.tsx
@@ -1,5 +1,77 @@
+import styled from "styled-components";
+import SubmitBtn from "../../../components/Common/SubmitBtn";
+import downArrow from "../../../assets/img/imgBtn/downArrow.svg";
+
 function BusRealTimeSelect() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <h1>셔틀버스 위치공유</h1>
+      <SelectContainer>
+        <label htmlFor="bus-number-selector">셔틀버스를 선택해주세요.</label>
+        <SelectWrapper>
+          <select name="number" id="bus-number-selector">
+            <option value="">1호차</option>
+            <option value="">2호차</option>
+            <option value="">3호차</option>
+            <option value="">4호차</option>
+            <option value="">5호차</option>
+            <option value="">6호차</option>
+          </select>
+          <div className="down-arrow-icon">
+            <img src={downArrow} alt="아래 화살표" />
+          </div>
+        </SelectWrapper>
+      </SelectContainer>
+      <SubmitBtn text="셔틀 위치 보기" link="/" />
+    </Wrapper>
+  );
 }
 
 export default BusRealTimeSelect;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    margin: 50px 0;
+    font-size: 26px;
+  }
+
+  h2 {
+    font-size: 20px;
+    margin: 10px 0;
+  }
+`;
+
+const SelectContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+`;
+
+const SelectWrapper = styled.div`
+  position: relative;
+
+  select {
+    width: 100%;
+    height: 40px;
+    margin: 10px 0;
+    padding: 10px;
+    font-size: 14px;
+    font-family: "Noto Sans KR", "Noto Sans", sans-serif;
+    border-radius: 5px;
+    outline: none;
+    appearance: none;
+    box-sizing: border-box;
+  }
+
+  .down-arrow-icon {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 10px;
+  }
+`;
