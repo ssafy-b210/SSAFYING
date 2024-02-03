e6ad09cb738e36ff51e67597a1927bfca603f419
임지은 <ljieun01@naver.com>
2024-02-04 00:59:41 +0900

    design: 유저 프로필 css 수정
    Change-Id: I2069facf21faf75110761a1928f34339fa79be6c

--- ssafying-fe/src/components/Profile/ProfileMain/ProfileIntroduction.tsx
+++ ssafying-fe/src/components/Profile/ProfileMain/ProfileIntroduction.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 function ProfileIntroduction() {
+  const data = {
+    intro:
+      "이애옹이올시다\n싸피 10기 팀제주도 팀장이라구요\naeong123@github.com",
+  };
+
   return (
     <StyledProfileIntroduction>
-      {`이애옹이올시다\n싸피 10기 팀제주도 팀장이라구요\naeong123@github.com`}
+      <div>{data.intro}</div>
+      {/* TODO : 포트폴리오 링크 추가 */}
     </StyledProfileIntroduction>
   );
 }
@@ -14,9 +20,12 @@ const StyledProfileIntroduction = styled.div`
   position: relative;
   display: inline-block;
   margin-bottom: 16px;
+  font-size: 14px;
   white-space: pre-wrap;
+  width: 100%;
+  box-sizing: border-box;
 
   @media screen and (min-width: 500px) {
-    margin-left: 45px;
+    padding-left: 32px;
   }
 `;
