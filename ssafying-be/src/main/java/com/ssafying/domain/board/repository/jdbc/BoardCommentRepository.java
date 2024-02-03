fc627dc11b23967f819865fedc7a6afcfa1a4002
Hgstudio <tndms0308@naver.com>
2024-02-04 02:44:33 +0900

    feat : BoardCommentRepository findByParentComment 추가

--- ssafying-be/src/main/java/com/ssafying/domain/board/repository/jdbc/BoardCommentRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/repository/jdbc/BoardCommentRepository.java
@@ -3,5 +3,9 @@ package com.ssafying.domain.board.repository.jdbc;
 import com.ssafying.domain.board.entity.BoardComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardCommentRepository extends JpaRepository<BoardComment, Integer> {
+
+    List<BoardComment> findByParentComment(BoardComment parentComment);
 }
