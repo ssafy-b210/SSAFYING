59d0665a7ea0c64357d66270774b5f874c29afae
김혜진 <hyejin9203@naver.com>
2024-02-04 14:32:34 +0900

    feat: tmap api 스크립트 코드 수정
    Change-Id: Ia0d22141692feb6fb7a00c6860206b284ce830c9

--- ssafying-fe/src/index.tsx
+++ ssafying-fe/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOMClient from "react-dom/client";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { TMAP_API_KEY } from "./apis/constants";
@@ -9,19 +8,20 @@ import { loadTMAPScript } from "./apis/api/shuttle/tmap";
 loadTMAPScript(TMAP_API_KEY)
   .then(() => {
     // TMAP 스크립트가 로드된 후에 리액트 앱을 렌더링
-    ReactDOM.render(<App />, document.getElementById('root'));
+    const root = ReactDOM.createRoot(
+      document.getElementById("root") as HTMLElement
+    );
+    root.render(
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    );
+    
+
   })
   .catch(error => {
     console.error('Failed to load TMAP script:', error);
   });
 
-const root = ReactDOMClient.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
