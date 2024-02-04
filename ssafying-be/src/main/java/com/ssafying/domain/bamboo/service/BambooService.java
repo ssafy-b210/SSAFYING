1e842c9fae136d666d88751950081e35a109b8ed
Hgstudio <tndms0308@naver.com>
2024-02-05 02:22:18 +0900

    feat : BambooService addBambooComment 구현

--- ssafying-be/src/main/java/com/ssafying/domain/bamboo/service/BambooService.java
+++ ssafying-be/src/main/java/com/ssafying/domain/bamboo/service/BambooService.java
@@ -1,9 +1,15 @@
 package com.ssafying.domain.bamboo.service;
 
+import com.ssafying.domain.bamboo.dto.request.AddBambooCommentRequest;
 import com.ssafying.domain.bamboo.dto.request.AddBambooRequest;
 import com.ssafying.domain.bamboo.dto.response.FindDetailBambooResponse;
 import com.ssafying.domain.bamboo.dto.response.FindListBambooResponse;
+import com.ssafying.domain.bamboo.entity.Bamboo;
+import com.ssafying.domain.bamboo.entity.BambooComment;
+import com.ssafying.domain.bamboo.repository.jdbc.BambooCommentRepository;
 import com.ssafying.domain.bamboo.repository.jdbc.BambooRepository;
+import com.ssafying.domain.user.entity.User;
+import com.ssafying.domain.user.repository.jdbc.UserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,8 @@ import java.util.List;
 public class BambooService {
 
     private final BambooRepository bambooRepository;
+    private final UserRepository userRepository;
+    private final BambooCommentRepository bambooCommentRepository;
 
     /**
      * 4.1 대나무숲 조회
@@ -32,6 +40,7 @@ public class BambooService {
      */
     @Transactional
     public int addBamboo(@Valid AddBambooRequest request) {
+
 
         return 0;
     }
@@ -49,9 +58,27 @@ public class BambooService {
      * 4.4 대나무숲 댓글 작성
      */
     @Transactional
-    public int addBambooComment(Long bambooId) {
+    public Long addBambooComment(Long bambooId, AddBambooCommentRequest request) {
 
-        return 0;
+        // 작성하려는 유저가 존재하는지 확인
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("유저가 없습니다."));
+
+        // 작성하려는 대나무숲 글이 있는지 확인
+        Bamboo bamboo = bambooRepository.findById(bambooId)
+                .orElseThrow(() -> (new RuntimeException("대나무숲 글이 존재하지 않습니다.")));
+
+        // 대나무숲 글이 존재한다면 댓글을 작성함
+        BambooComment bambooComment = BambooComment.createBambooComment(
+                bamboo,
+                user,
+                request.getContent()
+        );
+
+        BambooComment save = bambooCommentRepository.save(bambooComment);
+
+
+        return save.getId();
     }
 
     /**
