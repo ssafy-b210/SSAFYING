4a03b8b8dea6c67a739a8b6f4b9ac2774366d03b
Hgstudio <tndms0308@naver.com>
2024-02-04 20:06:27 +0900

    feat : FindDetailBoardResponse DTO 생성

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/response/FindDetailBoardResponse.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/response/FindDetailBoardResponse.java
@@ -1,6 +1,29 @@
 package com.ssafying.domain.board.dto.response;
 
-public class FindDetailBoardResponse {
+import com.ssafying.domain.board.dto.ParentCommentDTO;
+import com.ssafying.domain.board.entity.CategoryStatus;
+import lombok.Builder;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Getter
+public class FindDetailBoardResponse {
+    String userName;
+
+    String title;
+
+    String content;
+
+    CategoryStatus category;
+
+    boolean isAnonymous;
+
+    LocalDateTime createAt;
+
+    List<ParentCommentDTO> comments = new ArrayList<>();
 
 }
