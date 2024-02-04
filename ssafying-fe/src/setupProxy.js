8c11da7efbf76dd77042ecce98c32da5a8fdce2d
김혜진 <hyejin9203@naver.com>
2024-02-05 01:58:49 +0900

    init: jpg 추가
    Change-Id: Ib5cdee3c4884b3f110898d203d6fd547381c697c

--- ssafying-fe/src/setupProxy.js
+++ ssafying-fe/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     '/job-search', // 프록시 요청을 보낼 엔드포인트
     createProxyMiddleware({
-      target: "https://oapi.saramin.co.kr",
+      target: REACT_APP_SARAMIN_BASE_URL,
       changeOrigin: true,
     })
   );
