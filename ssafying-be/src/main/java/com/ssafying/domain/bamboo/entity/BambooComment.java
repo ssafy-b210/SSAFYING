6a1b75859781fe189ff7ea6d43763224840eae7d
Hgstudio <tndms0308@naver.com>
2024-02-04 20:37:38 +0900

    feat : BambooComment Entity 생성

---
+++ ssafying-be/src/main/java/com/ssafying/domain/bamboo/entity/BambooComment.java
@@ -0,0 +1,29 @@
+package com.ssafying.domain.bamboo.entity;
+
+import com.ssafying.domain.user.entity.User;
+import com.ssafying.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+
+public class BambooComment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bamboo_comment_id")
+    private Long id; //대나무숲 댓글 id
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bamboo_id")
+    private Bamboo bamboo; //댓글 단 대나무숲 글
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uesr_id")
+    private User user; //댓글 단 회원
+
+    private String content; //댓글 내용
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private BambooComment parentComment; //부모댓글
+
+    //생성일자와 수정일자는 BaseTimeEntity에 있음
+}
