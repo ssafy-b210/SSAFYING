8424a08f3f657fdbee8dedbdc2c715a4ea6ee037
Hgstudio <tndms0308@naver.com>
2024-02-05 16:04:05 +0900

    feat : FindDetailBambooResponse 추가

--- ssafying-be/src/main/java/com/ssafying/domain/bamboo/dto/response/FindDetailBambooResponse.java
+++ ssafying-be/src/main/java/com/ssafying/domain/bamboo/dto/response/FindDetailBambooResponse.java
@@ -1,4 +1,19 @@
 package com.ssafying.domain.bamboo.dto.response;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
 public class FindDetailBambooResponse {
+
+    String content;
+
+    LocalDateTime createdAt;
+
+    List<BambooCommentResponse> comments;
 }
