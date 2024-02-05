bd5ef84ddaa92ea439c92abf2810531278253bdd
tndms0308 <tndms0308@naver.com>
2024-02-06 02:35:09 +0900

    feat: FindListBambooResponse 작성

--- ssafying-be/src/main/java/com/ssafying/domain/bamboo/dto/response/FindListBambooResponse.java
+++ ssafying-be/src/main/java/com/ssafying/domain/bamboo/dto/response/FindListBambooResponse.java
@@ -1,4 +1,21 @@
 package com.ssafying.domain.bamboo.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FindListBambooResponse {
+
+    String content;
+
+    LocalDateTime createdAt;
+
+    Long countComment;
 }
