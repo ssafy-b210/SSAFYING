786af0b631c6facd016f9108d3e49a4e69bb1bd2
tndms0308 <tndms0308@naver.com>
2024-02-06 02:35:22 +0900

    feat: testDTO 작성

---
+++ ssafying-be/src/main/java/com/ssafying/domain/bamboo/dto/testDTO.java
@@ -0,0 +1,15 @@
+package com.ssafying.domain.bamboo.dto;
+
+import com.ssafying.domain.bamboo.dto.response.FindListBambooResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class testDTO {
+    List<FindListBambooResponse> title;
+
+
+}
