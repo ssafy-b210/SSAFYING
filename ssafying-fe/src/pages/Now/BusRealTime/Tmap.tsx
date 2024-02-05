719697c9f05f147b5ddcb1eb6acf143ca47250d6
임지은 <ljieun01@naver.com>
2024-02-05 17:05:00 +0900

    feat: 현재 위치에 마커 생성하는 기능 추가
    Change-Id: I7423dd387c2458054956f993d86d102092c9c950

--- ssafying-fe/src/pages/Now/BusRealTime/Tmap.tsx
+++ ssafying-fe/src/pages/Now/BusRealTime/Tmap.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import markerImg from "../../../assets/img/imgBtn/dm.svg";
 
 declare global {
   interface Window {
@@ -9,21 +10,51 @@ declare global {
 function Tmap() {
   const { Tmapv3 } = window;
 
-  function initTmap() {
+  // 현재 위치 요청 후 지도 생성
+  function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      initTmap(latitude, longitude);
+    });
+  }
+
+  // 지도 생성
+  function initTmap(latitude: number, longitude: number) {
     const mapDiv = document.getElementById("map_div");
 
     if (!mapDiv?.firstChild) {
       const map = new window.Tmapv3.Map("map_div", {
-        center: new Tmapv3.LatLng(37.566481622437934, 126.98502302169841), // 지도 초기 좌표
+        center: new Tmapv3.LatLng(latitude, longitude),
         width: "890px",
         height: "400px",
         zoom: 15,
       });
+
+      //Marker 객체 생성.
+      const marker = new Tmapv3.Marker({
+        position: new Tmapv3.LatLng(latitude, longitude),
+        icon: markerImg,
+        map: map,
+      });
+
+      resizeMap();
+      map.resize();
+    }
+  }
+
+  //지도의 사이즈를 지정한 값만큼 변경할 수 있는 함수입니다.
+  function resizeMap() {
+    const mapResize = document.getElementById("map_div"); //map의 div
+
+    if (mapResize !== null) {
+      mapResize.style.width = "100%"; //map의 width 변경
+      mapResize.style.height = "400px"; //map의 height 변경
     }
   }
 
   useEffect(() => {
-    initTmap();
+    getCurrentPosition();
   }, []);
 
   return <div id="map_div"></div>;
