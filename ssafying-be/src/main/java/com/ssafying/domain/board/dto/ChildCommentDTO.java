2826455760abea7965dfe553eb65db2715b42799
tndms0308 <tndms0308@naver.com>
2024-02-07 04:02:41 +0900

    feat: ChildCommentDTO 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/ChildCommentDTO.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/ChildCommentDTO.java
@@ -1,4 +1,9 @@
 package com.ssafying.domain.board.dto;
 
+import java.time.LocalDateTime;
+
 public class ChildCommentDTO {
+    String comment;
+    String userName;
+    LocalDateTime createdAt;
 }
