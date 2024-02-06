c9ee79ac23a6ee5e5d77b47999956ccaa0943f7d
tndms0308 <tndms0308@naver.com>
2024-02-07 04:02:52 +0900

    feat: ParentCommentDTO 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/ParentCommentDTO.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/ParentCommentDTO.java
@@ -1,11 +1,12 @@
 package com.ssafying.domain.board.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ParentCommentDTO {
     //부모댓글, 댓글쓴유저, 자식댓글들
     String comment;
     String userName;
-
+    LocalDateTime createdAt;
     List<ChildCommentDTO> childCommentList;
 }
