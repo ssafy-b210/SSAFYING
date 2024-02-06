886936d49eddf23982536d061e00ec866ea13f44
Hgstudio <tndms0308@naver.com>
2024-02-06 18:00:39 +0900

    feat: BoardComment setBoard 메서드 추가

--- ssafying-be/src/main/java/com/ssafying/domain/board/entity/BoardComment.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/entity/BoardComment.java
@@ -65,4 +65,8 @@ public class BoardComment extends BaseTimeEntity {
     ) {
         this.content = content;
     }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 }
