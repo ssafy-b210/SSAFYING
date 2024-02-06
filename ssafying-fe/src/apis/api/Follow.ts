85061e2ff67125d114315e7d25493b7ea8f155a1
임지은 <ljieun01@naver.com>
2024-02-06 23:59:47 +0900

    feat: Follow, Profile axios 코드 작성
    Change-Id: I16f20d12f800ccc7f097f8ef1e750daed45ac94d

---
+++ ssafying-fe/src/apis/api/Follow.ts
@@ -0,0 +1,84 @@
+import { axios } from "../utils/axios";
+
+const REST_USERS_API = `/api/users`;
+
+// 팔로잉 리스트 조회
+export async function selectFollowingList(userId: number) {
+  try {
+    const response = await axios.get(`${REST_USERS_API}/following/${userId}`);
+    console.log(response);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 팔로워 리스트 조회
+export async function selectFollowerList(userId: number) {
+  try {
+    const response = await axios.get(`${REST_USERS_API}/followers/${userId}`);
+    console.log(response);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 팔로잉 리스트 검색 결과 조회
+export async function searchFollowingList(userId: number, nickname: string) {
+  try {
+    const response = await axios.get(`${REST_USERS_API}/following`, {
+      data: {
+        userId,
+        nickname,
+      },
+    });
+    console.log(response);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 팔로워 리스트 검색 결과 조회
+export async function searchFollowerList(userId: number, nickname: string) {
+  try {
+    const response = await axios.get(`${REST_USERS_API}/follower`, {
+      data: {
+        userId,
+        nickname,
+      },
+    });
+    console.log(response);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 팔로우하기
+export async function followUser(userId: number) {
+  try {
+    const response = await axios.post(`${REST_USERS_API}/following`, {
+      data: {
+        userId,
+      },
+    });
+    console.log(response);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 언팔로우하기
+export async function unfollowUser(userId: number) {
+  try {
+    const response = await axios.delete(
+      `${REST_USERS_API}/following/${userId}`,
+      {
+        headers: {
+          refreshToken: "",
+        },
+      }
+    );
+    console.log(response);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
---
+++ ssafying-fe/src/apis/api/Profile.ts
@@ -0,0 +1,151 @@
+import { axios } from "../utils/axios";
+
+const REST_MYPAGE_API = `/api/mypage`;
+
+// 마이페이지의 유저 프로필 조회
+export async function selectMyPageDetail(userId: number) {
+  try {
+    const response = await axios.get(`${REST_MYPAGE_API}/${userId}`);
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 유저가 사용한 해시태그 전체 조회
+export async function selectHashtagList(userId: number) {
+  try {
+    const response = await axios.get(`${REST_MYPAGE_API}/${userId}/hashtags`);
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 유저가 작성한 피드 조회
+export async function selectMyFeedList(userId: number) {
+  try {
+    const response = await axios.get(`${REST_MYPAGE_API}/${userId}/feeds`);
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 유저가 작성한 게시글 조회
+export async function selectMyBoardList(userId: number) {
+  try {
+    const response = await axios.get(`${REST_MYPAGE_API}/${userId}/boards`);
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 유저가 스크랩한 피드 조회
+export async function selectSavedFeedList(userId: number) {
+  try {
+    const response = await axios.get(
+      `${REST_MYPAGE_API}/${userId}/feeds/scrap`
+    );
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 유저가 스크랩한 게시글 조회
+export async function selectSavedBoardList(userId: number) {
+  try {
+    const response = await axios.get(
+      `${REST_MYPAGE_API}/${userId}/boards/scrap`
+    );
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 유저가 스크랩한 채용공고 조회
+export async function selectSavedRecruitList(userId: number) {
+  try {
+    const response = await axios.get(
+      `${REST_MYPAGE_API}/${userId}/recruits/scrap`
+    );
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 포트폴리오 링크 조회
+export async function selectPortfolioLinkList(userId: number) {
+  try {
+    const response = await axios.get(`${REST_MYPAGE_API}/${userId}/portfolio`);
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 포트폴리오 링크 생성
+export async function createPortfolioLink(
+  userId: number,
+  portfolioUrl: string,
+  type: string
+) {
+  try {
+    const response = await axios.post(
+      `${REST_MYPAGE_API}/${userId}/portfolio`,
+      {
+        portfolioUrl,
+        type,
+      }
+    );
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 포트폴리오 링크 수정
+export async function modifyPortfolioLink(
+  portfolioId: number,
+  portfolioUrl: string,
+  type: string
+) {
+  try {
+    const response = await axios.put(
+      `${REST_MYPAGE_API}/portfolio/${portfolioId}`,
+      {
+        portfolioUrl,
+        type,
+      }
+    );
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 포트폴리오 링크 삭제
+export async function deletePortfolioLink(portfolioId: number) {
+  try {
+    const response = await axios.delete(
+      `${REST_MYPAGE_API}/portfolio/${portfolioId}`
+    );
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+// 포트폴리오 리드미 작성
+export async function modifyPortfolioReadMe(userId: number) {
+  try {
+    const response = await axios.put(`${REST_MYPAGE_API}/${userId}/readme`);
+    console.log(response.data);
+  } catch (e: any) {
+    console.log(e);
+  }
+}
