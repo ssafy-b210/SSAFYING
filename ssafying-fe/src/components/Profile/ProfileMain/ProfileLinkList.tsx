abf701837ca920781e71b72942df10959dc755ab
임지은 <ljieun01@naver.com>
2024-02-06 14:50:24 +0900

    feat: 바이오 링크 컴포넌트 추가
    Change-Id: Ie95a689fd43253e7d68a0a3dd5e2d956d10cebdc

--- ssafying-fe/src/components/Profile/ProfileMain/ProfileLinkList.tsx
+++ ssafying-fe/src/components/Profile/ProfileMain/ProfileLinkList.tsx
@@ -1,30 +1,124 @@
+import styled from "styled-components";
+import github from "../../../assets/img/MyPageIcons/github.svg";
+import notion from "../../../assets/img/MyPageIcons/notion.svg";
+import tistory from "../../../assets/img/MyPageIcons/tistory.svg";
+import blog from "../../../assets/img/MyPageIcons/blog.svg";
+import etc from "../../../assets/img/MyPageIcons/etc.svg";
+
 function ProfileLinkList() {
+  const icons = [
+    {
+      name: "github",
+      src: github,
+    },
+    {
+      name: "notion",
+      src: notion,
+    },
+    {
+      name: "tistory",
+      src: tistory,
+    },
+    {
+      name: "blog",
+      src: blog,
+    },
+    {
+      name: "etc",
+      src: etc,
+    },
+  ];
+
+  const testInfo = [
+    {
+      id: 1,
+      link: "https://github.com/aeong123",
+      type: "github",
+    },
+    {
+      id: 2,
+      link: "https://www.notion.so/d44f58470aa94c948f6cc9032f4aca3f",
+      type: "notion",
+    },
+    {
+      id: 3,
+      link: "https://aeong123.tistory.com",
+      type: "tistory",
+    },
+    {
+      id: 4,
+      link: "https://blog.naver.com/aeong123",
+      type: "blog",
+    },
+    {
+      id: 5,
+      link: "https://www.acmicpc.net/user/qlsjtm000",
+      type: "etc",
+    },
+  ];
+
   return (
-    <div>
-      <ul>
-        <li>
-          <img src="" alt="" />
-          <a
-            href="https://github.com/aeong123"
-            target="_blank"
-            rel="noreferrer"
-          >
-            github.com/aeong123
-          </a>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <a
-            href="https://aeong123.tistory.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            aeong123.tistory.com
-          </a>
-        </li>
-      </ul>
-    </div>
+    <LinkList>
+      {testInfo.map((data) => {
+        const idx = icons.findIndex((el) => el.name === data.type);
+        return (
+          <LinkListItem>
+            <a href={data.link} type="_blank" rel="noreferrer">
+              <IconWarpper>
+                <img src={icons[idx].src} alt="" />
+              </IconWarpper>
+              <div className="text-url">{data.link}</div>
+            </a>
+          </LinkListItem>
+        );
+      })}
+    </LinkList>
   );
 }
 
 export default ProfileLinkList;
+
+const LinkList = styled.ul`
+  margin: 0;
+  padding: 10px 0 10px;
+  border-top: 1px solid #9a9a9a;
+  list-style: none;
+
+  @media screen and (min-width: 500px) {
+    margin: 0 32px;
+  }
+`;
+
+const LinkListItem = styled.li`
+  padding-bottom: 10px;
+  font-size: 14px;
+
+  a {
+    display: flex;
+    margin-left: 10px;
+    color: #000;
+    text-decoration: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .text-url {
+    padding-left: 10px;
+  }
+`;
+
+const IconWarpper = styled.div`
+  width: 20px;
+  height: 20px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
