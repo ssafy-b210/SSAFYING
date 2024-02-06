bfd1010268420b504cb4b411ab5221f6c5921da8
임지은 <ljieun01@naver.com>
2024-02-06 15:33:49 +0900

    design: center header 추가
    Change-Id: I9d6b8ce84c7f80077c31e3adeb6da0f198e05226

--- ssafying-fe/src/pages/Profile/ProfileMain.tsx
+++ ssafying-fe/src/pages/Profile/ProfileMain.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import MyContentsContainer from "../../components/Profile/MyContents/MyContentsContainer";
 import ProflieContainer from "../../components/Profile/ProfileMain/ProfileContainer";
+import CenterHeader from "../../components/Common/CenterHeader";
 
 function ProfileMain() {
   return (
     <Wrapper>
+      <CenterHeader />
       <ProflieContainer />
       <MyContentsContainer />
     </Wrapper>
