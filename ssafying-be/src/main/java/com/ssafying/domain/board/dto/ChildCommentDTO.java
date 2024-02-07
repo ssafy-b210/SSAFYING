b3581eb624cdb1b3b16df314b22cd2c2e9898747
tndms0308 <tndms0308@naver.com>
2024-02-08 02:40:52 +0900

    feat: ChildCommentDTO 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/ChildCommentDTO.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/ChildCommentDTO.java
@@ -1,9 +1,37 @@
 package com.ssafying.domain.board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class ChildCommentDTO {
     String comment;
     String userName;
+    boolean isAnonymous;
     LocalDateTime createdAt;
+
+
+    public static ChildCommentDTO createChileComment(
+            String comment,
+            String userName,
+            boolean isAnonymous,
+            LocalDateTime createdAt
+
+    ) {
+        ChildCommentDTO childComment = new ChildCommentDTO();
+
+        childComment.comment = comment;
+        childComment.userName = userName;
+        childComment.isAnonymous = isAnonymous;
+        childComment.createdAt = createdAt;
+
+        return childComment;
+    }
 }
