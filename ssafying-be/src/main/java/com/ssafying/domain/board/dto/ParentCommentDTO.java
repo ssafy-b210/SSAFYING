5081a5fbdde3ce6a4da75ace6a7a1f7dd527a932
tndms0308 <tndms0308@naver.com>
2024-02-08 02:41:30 +0900

    feat: ParentCommentDTO 구현

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/ParentCommentDTO.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/ParentCommentDTO.java
@@ -1,12 +1,38 @@
 package com.ssafying.domain.board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class ParentCommentDTO {
     //부모댓글, 댓글쓴유저, 자식댓글들
     String comment;
     String userName;
+    boolean isAnonymous;
     LocalDateTime createdAt;
     List<ChildCommentDTO> childCommentList;
+
+    public static ParentCommentDTO createParentComment(
+            String comment,
+            String userName,
+            boolean isAnonymous,
+            LocalDateTime createdAt
+    ) {
+        ParentCommentDTO parentComment = new ParentCommentDTO();
+
+        parentComment.comment = comment;
+        parentComment.userName = userName;
+        parentComment.isAnonymous = isAnonymous;
+        parentComment.createdAt = createdAt;
+
+        return parentComment;
+    }
 }
