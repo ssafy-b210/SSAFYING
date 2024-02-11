96306c5c40576766c17e958e4539bf9f0618bc3f
tndms0308 <tndms0308@naver.com>
2024-02-12 05:24:58 +0900

    feat: VoteMealRequest 구현

---
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/dto/request/VoteMealRequest.java
@@ -0,0 +1,16 @@
+package com.ssafying.domain.meal.dto.request;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VoteMealRequest {
+
+    int userId;
+
+    int mealPlannerId;
+
+    int menuNum;
+}
