2df6be9bf782276d2c04c071bbf509b8461f8561
tndms0308 <tndms0308@naver.com>
2024-02-12 05:23:26 +0900

    feat: MealController 구현

---
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/controller/MealController.java
@@ -0,0 +1,62 @@
+package com.ssafying.domain.meal.controller;
+
+import com.ssafying.domain.meal.dto.request.AddMealRequest;
+import com.ssafying.domain.meal.dto.request.ModifyMealRequest;
+import com.ssafying.domain.meal.dto.request.VoteMealRequest;
+import com.ssafying.domain.meal.service.MealService;
+import com.ssafying.global.result.ResultResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/meal")
+@CrossOrigin("*")
+public class MealController {
+
+    private final MealService mealService;
+
+    /**
+     * 13.1 식단표 생성
+     */
+    @PostMapping
+    public ResponseEntity<ResultResponse<Integer>> mealAdd(
+            @RequestBody AddMealRequest request) {
+        int result = mealService.addMeal(request);
+
+        return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), result));
+    }
+
+
+    /**
+     * 13.2 식단표 메뉴 수정
+     */
+    @PatchMapping("/{mealPlannerId}")
+    public ResponseEntity<ResultResponse<Integer>> mealModify(
+            @PathVariable(name = "mealPlannerId") int mealPlannerId,
+            @RequestBody ModifyMealRequest request) {
+        int result = mealService.modifyMeal(mealPlannerId, request);
+
+        return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), result));
+    }
+
+
+    /**
+     * 13.3 식단표 메뉴 투표
+     */
+    @PostMapping("/vote")
+    public ResponseEntity<ResultResponse<Integer>> mealVote(
+            @RequestBody @Valid VoteMealRequest request
+    ) {
+        int result = mealService.voteMeal(request);
+
+        return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), result));
+    }
+
+    /**
+     * 13.4 식단표 조회
+     */
+}
