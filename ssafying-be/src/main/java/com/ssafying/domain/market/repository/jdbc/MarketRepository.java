15649decf6e31d85e10ecb5c75f0b0e7c4861375
2_yewon <2_yewon@naver.com>
2024-02-11 12:55:25 +0900

    chore: market 전체조회 response 수정
    Change-Id: Ieb48ed5f8e1755025d9d33cbdedcedbdac03d0ce

--- ssafying-be/src/main/java/com/ssafying/domain/market/repository/jdbc/MarketRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/market/repository/jdbc/MarketRepository.java
@@ -2,6 +2,15 @@ package com.ssafying.domain.market.repository.jdbc;
 
 import com.ssafying.domain.market.entity.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MarketRepository extends JpaRepository<Market, Integer> {
+
+    //거래 여부에 따른 필터링
+    @Query("select m from Market m where m.isSoldout = :isSoldout")
+    List<Market> findByIsSoldout(@Param("isSoldout") Boolean isSoldout);
+
 }
