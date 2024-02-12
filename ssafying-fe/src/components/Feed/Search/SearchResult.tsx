23a3fa8d5ec77bf24c9d0c2f237c199c2f100a13
DESKTOP-LL7FDJ6\SSAFY <hyejin9203@naver.com>
2024-02-13 01:40:34 +0900

    feat: 피드 검색 api 연결
    Change-Id: Ib938d6781539cc670e50bb6b8d9013bbd29e3ddf

--- ssafying-fe/src/components/Feed/Search/SearchResult.tsx
+++ ssafying-fe/src/components/Feed/Search/SearchResult.tsx
@@ -3,18 +3,22 @@ import SearchBar from "../utils/SearchBar";
 import UserItemList from "./UserItemList";
 import HashSearchList from "./HashSearchList";
 import { getFeedSearch } from "../../../apis/api/Feed";
+import FeedListItem from "../FeedMain/FeedListItem";
 
 function SearchResult() {
   const [hashtag, setHashtag] = useState("");
   const [user, setUser] = useState("");
+  const [feedList, setFeedList] = useState<any[]>([]);
+
+  console.log(feedList);
 
   const handleSearchChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
     if (e.target.value.charAt(0) === "#") {
       setHashtag(e.target.value);
       try {
-        const res = await getFeedSearch(e.target.value, user);
-        console.log(res);
+        const res = await getFeedSearch(e.target.value.substring(1), user);
+        setFeedList(res || []);
       } catch (error) {
         console.error(error);
       }
@@ -22,7 +26,7 @@ function SearchResult() {
       setUser(e.target.value);
       try {
         const res = await getFeedSearch(hashtag, e.target.value);
-        console.log(res);
+        setFeedList(res || []);
       } catch (error) {
         console.error(error);
       }
@@ -31,7 +35,14 @@ function SearchResult() {
   return (
     <div>
       <SearchBar onChange={handleSearchChange} />
-      <UserItemList
+      {feedList.length > 0 && (
+        <div className="feed-list">
+          {feedList.map((item) => (
+            <FeedListItem feed={item} />
+          ))}
+        </div>
+      )}
+      {/* <UserItemList
         userList={[
           {
             userId: "aeong",
@@ -50,7 +61,7 @@ function SearchResult() {
           { hashId: "1", hashTag: "대전" },
           { hashId: "2", hashTag: "점심" },
         ]}
-      />
+      /> */}
     </div>
   );
 }
