25b643c2a6775b4868f601c6d598ceeab9745874
2_yewon <2_yewon@naver.com>
2024-02-13 13:07:41 +0900

    feat: 회원 닉네임으로 검색 api 추가
    Change-Id: Ia3f59db837258a6ec7779fe6b631546614477698

--- ssafying-be/src/main/java/com/ssafying/domain/shuttle/controller/ShuttleController.java
+++ ssafying-be/src/main/java/com/ssafying/domain/shuttle/controller/ShuttleController.java
@@ -2,10 +2,9 @@ package com.ssafying.domain.shuttle.controller;
 
 import com.ssafying.domain.shuttle.dto.request.UserLocationRequest;
 import com.ssafying.domain.shuttle.dto.response.BusStopListResponse;
-import com.ssafying.domain.shuttle.entity.BusStop;
 import com.ssafying.domain.shuttle.entity.Shuttle;
 import com.ssafying.domain.shuttle.service.BusStopService;
-import com.ssafying.domain.shuttle.service.LocationMessageService;
+import com.ssafying.domain.shuttle.service.ShuttleService;
 import com.ssafying.global.result.ResultResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -22,13 +21,13 @@ import java.util.List;
 @CrossOrigin("*")
 public class ShuttleController {
 
-    private final LocationMessageService locationMessageService;
+    private final ShuttleService shuttleService;
     private final BusStopService busStopService;
 
     @MessageMapping("/location/{shuttleId}") //pub이 자동으로 붙음
     public void locationSend(UserLocationRequest request){
 
-        locationMessageService.sendLocationMessage(request);
+        shuttleService.sendLocationMessage(request);
 
     }
 
