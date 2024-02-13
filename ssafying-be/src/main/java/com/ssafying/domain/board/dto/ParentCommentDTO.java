3d2a0e3794f91f7f32b12ab8d418c77b8e919b72
tndms0308 <tndms0308@naver.com>
2024-02-14 00:49:58 +0900

    fix: ParentCommentDTO nickname 추가

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/ParentCommentDTO.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/ParentCommentDTO.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class ParentCommentDTO {
     //부모댓글, 댓글쓴유저, 자식댓글들
     String comment;
+    String nickname;
     String userName;
     boolean isAnonymous;
     LocalDateTime createdAt;
