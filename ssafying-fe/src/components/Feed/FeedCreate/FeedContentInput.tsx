bc7de29023c2bb95a23421cfb6d4cad0043a0212
이수민 <judy3504@naver.com>
2024-02-13 12:52:04 +0900

    feat: 피드 사진 firebase에 저장
    Change-Id: I6d95365b0e0f622a1b8810f9e0a9c641fd8de971

--- ssafying-fe/src/components/Feed/FeedCreate/FeedContentInput.tsx
+++ ssafying-fe/src/components/Feed/FeedCreate/FeedContentInput.tsx
@@ -6,12 +6,17 @@ import SelectHashtag from "./SelectHashtag";
 import { createFeedItem } from "../../../apis/api/Feed";
 import { useAppSelector } from "../../../store/hooks";
 import { selectUser } from "../../../store/reducers/user";
+import { fstorage } from "../../../apis/firebase";
+import { uploadString, ref, getDownloadURL } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 function FeedContentInput() {
   const [content, setContent] = useState(""); // TextArea의 내용을 저장할 상태
   const [images, setImages] = useState<string[]>([]); // 이미지들의 URL을 저장할 상태
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
   const [hashtags, setHashtags] = useState<string[]>([]); // 해시태그들 저장할 상태
   const user = useAppSelector(selectUser);
+  const navigate = useNavigate();
 
   console.log(hashtags);
 
@@ -19,15 +24,32 @@ function FeedContentInput() {
     try {
       if (content === "") {
         alert("문구를 입력해주세요");
-      } else if (hashtags.length === 0) {
-        alert("태그를 선택해주세요");
-      } else {
-        await createFeedItem(user.userId, content, images, hashtags);
-        setContent("");
-        setImages([]);
-        setHashtags([]);
-        alert("글이 성공적으로 저장되었습니다.");
+        return;
       }
+      if (hashtags.length === 0) {
+        alert("태그를 선택해주세요");
+        return;
+      }
+      // 이미지 업로드 및 URL 가져오기
+      const downloadURLs = await Promise.all(
+        imageUrls.map(async (imageUrl) => {
+          const response = await fetch(imageUrl);
+          const blob = await response.blob();
+          const fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+          const fileRef = ref(fstorage, `${user.nickname}/${fileName}`);
+          await uploadString(fileRef, imageUrl, "data_url");
+          const downloadURL = await getDownloadURL(fileRef);
+          console.log(downloadURL);
+          return downloadURL;
+        })
+      );
+
+      await createFeedItem(user.userId, content, downloadURLs, hashtags);
+      setContent("");
+      setImages([]);
+      setHashtags([]);
+      alert("글이 성공적으로 저장되었습니다.");
+      navigate("/feedhome");
     } catch (error) {
       console.error("글 저장 중 오류 발생:", error);
       alert("글 저장 중 오류가 발생했습니다. 다시 시도해주세요.");
@@ -36,7 +58,8 @@ function FeedContentInput() {
 
   return (
     <InputWrapper>
-      <ImgEdit onImagesChange={setImages} />
+      {/* <ImgEdit onImagesChange={setImages} /> */}
+      <ImgEdit onImagesChange={setImageUrls} />
       <TextWrapper>
         <TextArea
           value={content}
