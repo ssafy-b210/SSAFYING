d24e09d1bebc75afb3a7f1f305fbc1cdacbab15d
tndms0308 <tndms0308@naver.com>
2024-02-13 23:20:41 +0900

    feat: EmitterRepository 추가

---
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/repository/EmitterRepository.java
@@ -0,0 +1,27 @@
+package com.ssafying.domain.alert.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+@RequiredArgsConstructor
+public class EmitterRepository {
+
+    private final Map<Integer, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    public void save(int id, SseEmitter emitter) {
+        emitters.put(id, emitter);
+    }
+
+    public void deleteById(int userId) {
+        emitters.remove(userId);
+    }
+
+    public SseEmitter get(int userId) {
+        return emitters.get(userId);
+    }
+}
