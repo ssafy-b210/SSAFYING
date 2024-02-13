3a2360bc3dac0c113f98c739c6396de6844ed72a
DESKTOP-LL7FDJ6\SSAFY <hyejin9203@naver.com>
2024-02-13 14:18:43 +0900

    feat: 피드 검색
    Change-Id: Ibd064d71a4b7a6a93eb7b41e61f1ab05c7b04502

--- ssafying-fe/src/components/Feed/FeedMain/FeedListItem.tsx
+++ ssafying-fe/src/components/Feed/FeedMain/FeedListItem.tsx
@@ -27,7 +27,7 @@ function FeedListItem({ feed }: FeedProps) {
         feed.feedImages.map((image: any, index: number) => (
           <FeedListItemImg key={index} imageUrls={image} />
         ))} */}
-      <FeedListItemBtn likeCount={feed.likeCount} />
+      <FeedListItemBtn likeCount={feed.likeCount} feedId={feed.id} />
     </FeedListItemWrapper>
   );
 }
