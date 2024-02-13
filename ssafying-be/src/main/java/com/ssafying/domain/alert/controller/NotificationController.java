62d3f6c42ff1aac59420cb98a878bcca297f2412
tndms0308 <tndms0308@naver.com>
2024-02-13 23:21:11 +0900

    feat: NotificationController로 변경

--- ssafying-be/src/main/java/com/ssafying/domain/alert/controller/NotificationController.java
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/controller/NotificationController.java
@@ -1,7 +1,7 @@
 package com.ssafying.domain.alert.controller;
 
+import com.ssafying.domain.alert.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +9,13 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
-public class AlertController {
+public class NotificationController {
 
-    private final SseEmitters sseEmitters;
+    private final NotificationService notificationService;
 
-    @GetMapping("/connect/{userId}")
-    public ResponseEntity<SseEmitter> connect(
-            @PathVariable("userId") int userId) {
-        SseEmitter emitter = sseEmitters.subscribe(userId);
-
-        return ResponseEntity.ok(emitter);
+    @GetMapping(value = "/subscribe/{user_id}", produces = "text/event-stream;charset=UTF-8")
+    public SseEmitter subscribe(@PathVariable(value = "user_id") int userId) {
+        return notificationService.subscribe(userId);
     }
+
 }
