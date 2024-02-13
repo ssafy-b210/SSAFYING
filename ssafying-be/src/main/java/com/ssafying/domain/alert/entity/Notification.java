3b6e06af39be2bda6fe7972b143b4b88a63907ca
tndms0308 <tndms0308@naver.com>
2024-02-14 04:37:47 +0900

    feat: Notification Entity 구현

---
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/entity/Notification.java
@@ -0,0 +1,43 @@
+package com.ssafying.domain.alert.entity;
+
+import com.ssafying.domain.chat.entity.ChatRoom;
+import com.ssafying.domain.feed.entity.Feed;
+import com.ssafying.domain.user.entity.User;
+import com.ssafying.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id; // 알림 id
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User senderId; // 알림 보내는 사람 id
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiverId; // 알림 받는 사람 id
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "notification_type")
+    private NotificationTypeStatus notificationType; // 알림 종류
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoomId; // 채팅방 id
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feedId; // 피드 id // dm 보낸 경우 채팅방 id 저장
+
+    private Boolean isRead; // 알림 확인 여부 // like, comment 알림인 경우 feed id 저장
+
+
+}
