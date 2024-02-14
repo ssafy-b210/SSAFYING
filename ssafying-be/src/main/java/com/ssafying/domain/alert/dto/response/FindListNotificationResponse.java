28777ffdabb84befa9b853f5ee813ab36bd19137
Hgstudio <tndms0308@naver.com>
2024-02-14 22:17:48 +0900

    feat: FindListNotificationResponse 생성

--- ssafying-be/src/main/java/com/ssafying/domain/alert/dto/response/FindListNotificationResponse.java
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/dto/response/FindListNotificationResponse.java
@@ -1,4 +1,24 @@
 package com.ssafying.domain.alert.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class FindListNotificationResponse {
+    int receiverId;
+
+    String nickname;
+
+    String imgUrl;
+
+    LocalDateTime createdAt;
+
+    Long feedId;
 }
