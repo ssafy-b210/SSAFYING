9a10763971383d9cb654b55a875e132d850ac285
Hgstudio <tndms0308@naver.com>
2024-02-14 13:32:51 +0900

    fix: AddBoardCommentRequest 익명 여부 false로 @Builder.Default 초기화

--- ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/AddBoardCommentRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/board/dto/request/AddBoardCommentRequest.java
@@ -1,6 +1,5 @@
 package com.ssafying.domain.board.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
@@ -9,14 +8,12 @@ import lombok.*;
 @Getter
 public class AddBoardCommentRequest {
 
-    @NotNull
     int userId; //댓글 작성한 유저
 
-    @NotNull
     String content; //댓글 내용
 
-    @NotNull
-    Boolean isAnonymous; //익명 여부
+    @Builder.Default
+    Boolean isAnonymous = false; //익명 여부
 
     //    @NotNull
     int parentId; //부모 댓글
