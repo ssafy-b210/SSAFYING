5c74820c1be194061e00d95282ce9196471ffaa9
Hgstudio <tndms0308@naver.com>
2024-02-15 04:08:20 +0900

    fix: AddMealRequest 에서 mealPlannerDate 칼럼 삭제

--- ssafying-be/src/main/java/com/ssafying/domain/meal/dto/request/AddMealRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/dto/request/AddMealRequest.java
@@ -4,8 +4,6 @@ import com.ssafying.domain.meal.entity.MealOrderStatus;
 import com.ssafying.domain.shuttle.entity.CampusRegion;
 import lombok.*;
 
-import java.time.LocalDate;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,10 +12,8 @@ public class AddMealRequest {
 
     private CampusRegion campusRegion; // 캠퍼스 지역
 
-    private LocalDate mealPlannerDate; // 식단표 날짜
-
     private MealOrderStatus mealOrder; // 몇번째 메뉴인지
 
-    private String menu; // 식단표 메뉴
+    private String menu; // 식단표 메뉴 (imgUrl)
 
 }
