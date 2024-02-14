819ce9cd625b491273d687e47efdb9885e3fd117
Hgstudio <tndms0308@naver.com>
2024-02-14 13:13:20 +0900

    fix: NotificationController 어노테이션 추가

--- ssafying-be/src/main/java/com/ssafying/domain/alert/controller/NotificationController.java
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/controller/NotificationController.java
@@ -2,20 +2,30 @@ package com.ssafying.domain.alert.controller;
 
 import com.ssafying.domain.alert.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/notifications")
+@CrossOrigin("*")
 public class NotificationController {
 
     private final NotificationService notificationService;
 
+    /**
+     * 8.1 알림을 위한 SSE 연결
+     *
+     * @param userId
+     * @return
+     */
     @GetMapping(value = "/subscribe/{user_id}", produces = "text/event-stream;charset=UTF-8")
     public SseEmitter subscribe(@PathVariable(value = "user_id") int userId) {
         return notificationService.subscribe(userId);
     }
+
+    /**
+     * 8.2 알림 전체 조회
+     */
 
 }
