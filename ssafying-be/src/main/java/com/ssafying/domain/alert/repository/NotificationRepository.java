f760811c7068014dc096973b03acf4bc208cbf95
2_yewon <2_yewon@naver.com>
2024-02-14 16:39:28 +0900

    feat: NotificationRepository findByReceiverId 쿼리 추가
    Change-Id: I1d076699efbb6161af35483f96d55b9816622011

--- ssafying-be/src/main/java/com/ssafying/domain/alert/repository/NotificationRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/repository/NotificationRepository.java
@@ -1,9 +1,19 @@
 package com.ssafying.domain.alert.repository;
 
 import com.ssafying.domain.alert.entity.Notification;
+import com.ssafying.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 
+//    @Query("""
+//            select n
+//            from notifications n
+//            where
+//            """)
+    List<Notification> findByReceiverId(User receiverUser);
 }
