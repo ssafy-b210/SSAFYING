8a33a8693820fb5798818fc656657710528cd860
Hgstudio <tndms0308@naver.com>
2024-02-15 04:13:27 +0900

    feat: MealVoteRepository 에서 findByUserAndMealPlannerAndOrder, findByUserAndMealPlanner 생성

--- ssafying-be/src/main/java/com/ssafying/domain/meal/repository/jdbc/MealVoteRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/repository/jdbc/MealVoteRepository.java
@@ -1,11 +1,14 @@
 package com.ssafying.domain.meal.repository.jdbc;
 
+import com.ssafying.domain.meal.entity.MealOrderStatus;
 import com.ssafying.domain.meal.entity.MealPlanner;
 import com.ssafying.domain.meal.entity.MealVote;
 import com.ssafying.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MealVoteRepository extends JpaRepository<MealVote, Integer> {
 
@@ -14,6 +17,9 @@ public interface MealVoteRepository extends JpaRepository<MealVote, Integer> {
             from MealVote mv
             where mv.user = :user
             and mv.mealPlanner = :mealPlanner
+            and mv.order = :order
             """)
-    public int findByUserAndMealPlanner(@Param("user") User user, @Param("mealPlanner") MealPlanner mealPlanner);
+    public int findByUserAndMealPlannerAndOrder(@Param("user") User user, @Param("mealPlanner") MealPlanner mealPlanner, @Param("order") MealOrderStatus order);
+
+    Optional<MealVote> findByUserAndMealPlanner(User user, MealPlanner mealPlanner);
 }
