206c9518e82a1352ab3516f757e6c77e2ff9118e
Hgstudio <tndms0308@naver.com>
2024-02-14 13:12:31 +0900

    fix: Notification Entity createNotification 메서드 생성

--- ssafying-be/src/main/java/com/ssafying/domain/alert/entity/Notification.java
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/entity/Notification.java
@@ -1,6 +1,5 @@
 package com.ssafying.domain.alert.entity;
 
-import com.ssafying.domain.chat.entity.ChatRoom;
 import com.ssafying.domain.feed.entity.Feed;
 import com.ssafying.domain.user.entity.User;
 import com.ssafying.global.entity.BaseTimeEntity;
@@ -29,9 +28,9 @@ public class Notification extends BaseTimeEntity {
     @Column(name = "notification_type")
     private NotificationTypeStatus notificationType; // 알림 종류
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_room_id")
-    private ChatRoom chatRoomId; // 채팅방 id
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "chat_room_id")
+//    private ChatRoom chatRoomId; // 채팅방 id
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id")
@@ -40,4 +39,19 @@ public class Notification extends BaseTimeEntity {
     private Boolean isRead; // 알림 확인 여부 // like, comment 알림인 경우 feed id 저장
 
 
+    public static Notification createNotification(
+            User senderId,
+            User receiverId,
+            NotificationTypeStatus notificationType,
+            Feed feedId
+    ) {
+        Notification notification = new Notification();
+
+        notification.senderId = senderId;
+        notification.receiverId = receiverId;
+        notification.notificationType = notificationType;
+        notification.feedId = feedId;
+
+        return notification;
+    }
 }
