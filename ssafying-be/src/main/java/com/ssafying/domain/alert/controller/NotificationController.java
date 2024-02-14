5e8aa466b4e0bace568c949e9a950fb6caf95662
2_yewon <2_yewon@naver.com>
2024-02-14 16:39:13 +0900

    feat: NotificationController 알림 전체 조회 추가
    Change-Id: I0b69afbcdd50e63616294b76253b810242b9914c

--- ssafying-be/src/main/java/com/ssafying/domain/alert/controller/NotificationController.java
+++ ssafying-be/src/main/java/com/ssafying/domain/alert/controller/NotificationController.java
@@ -1,9 +1,17 @@
 package com.ssafying.domain.alert.controller;
 
+import com.ssafying.domain.alert.dto.response.FindListNotificationResponse;
 import com.ssafying.domain.alert.service.NotificationService;
+import com.ssafying.domain.bamboo.dto.response.FindListBambooResponse;
+import com.ssafying.domain.bamboo.dto.testDTO;
+import com.ssafying.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +35,13 @@ public class NotificationController {
     /**
      * 8.2 알림 전체 조회
      */
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResultResponse<List<FindListNotificationResponse>>> notificationList(
+            @PathVariable(value = "userId") int userId
+    ) {
+        List<FindListNotificationResponse> result = notificationService.findListNotification(userId);
+
+        return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), result));
+    }
 
 }
