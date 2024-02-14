02e6b3551e92fe6fdb68a1da17f84f3b8a8f2bca
Hgstudio <tndms0308@naver.com>
2024-02-15 04:11:53 +0900

    feat: MealService 추가

--- ssafying-be/src/main/java/com/ssafying/domain/meal/service/MealService.java
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/service/MealService.java
@@ -3,6 +3,7 @@ package com.ssafying.domain.meal.service;
 import com.ssafying.domain.meal.dto.request.AddMealRequest;
 import com.ssafying.domain.meal.dto.request.ModifyMealRequest;
 import com.ssafying.domain.meal.dto.request.VoteMealRequest;
+import com.ssafying.domain.meal.dto.response.FindMealPlannerResponse;
 import com.ssafying.domain.meal.entity.MealPlanner;
 import com.ssafying.domain.meal.entity.MealVote;
 import com.ssafying.domain.meal.repository.jdbc.MealRepository;
@@ -14,6 +15,11 @@ import com.ssafying.domain.user.repository.jdbc.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -31,17 +37,28 @@ public class MealService {
     @Transactional
     public int addMeal(AddMealRequest request) {
 
-        //TODO 해당 날짜로 이미 식단표가 생성되어있다면 익셉션 발생
-        // 해당 날짜로 이미 식단표가 있는 경우라면 add 대신 modify를 진행해야함
-
         // request 로 넘어온 캠퍼스 지역을 찾음
         Campus campus = campusRepository.findByCampusRegion(request.getCampusRegion())
                 .orElseThrow(() -> (new RuntimeException("해당 캠퍼스 지역이 존재하지 않습니다.")));
 
+        // 해당 날짜로 이미 식단표가 생성되어있다면 익셉션 발생
+        // 해당 날짜로 이미 식단표가 있는 경우라면 add 대신 modify를 진행해야함
+        Optional<MealPlanner> find = mealRepository.findByMealPlannerDateAndCampusAndMealOrder(
+                LocalDate.now(),
+                campus,
+                request.getMealOrder()
+        );
+
+        if (find.isPresent()) {
+            throw new RuntimeException("Found meal planner is not null.");
+        }
+
+        System.out.println("LocalDate.now() = " + LocalDate.now());
+        
         // 디비에 저장할 MealPlanner 준비
         MealPlanner meal = MealPlanner.createMeal(
                 campus,
-                request.getMealPlannerDate(),
+                LocalDate.now(),
                 request.getMealOrder(),
                 request.getMenu(),
                 0
@@ -86,19 +103,31 @@ public class MealService {
 
         // 해당 유저가 해당 식단표 메뉴에 투표를 이미 했는지 확인
         // TODO 그 날에 해당하는 식단표 메뉴 A 혹은 B에 이미 투표했다면 취소를 해줘야할 듯???
-        int findVote = mealVoteRepository.findByUserAndMealPlanner(user, mealPlanner);
+        int findVote = mealVoteRepository.findByUserAndMealPlannerAndOrder(user, mealPlanner, request.getOrder());
         if (findVote > 0) throw new RuntimeException("이미 해당 메뉴에 투표하였습니다.");
 
-        //TODO 해당 메뉴는 아니더라도 해당 날에 다른 식단표에 투표를 이미 했다면
+        //TODO 해당 메뉴는 아니더라도 해당 날에 다른 식단표에 투표를 이미 했다면 (order만 다름)
         // 그 식단표 mealVote 데이터를 없애고 해당 mealPlanner의 투표값도 -1 해줘야 할 듯 ..???
+        Optional<MealVote> mealVote = mealVoteRepository.findByUserAndMealPlanner(user, mealPlanner);
+        if (mealVote.isPresent()) {
 
+            // 같은 날에 다른 메뉴 투표한 경우이므로
+            // 투표했던 mealPlanner의 vote-1 해주고
+            // 새로운 거 vote+1 해주고
+            // 저 경우는 지움
+
+
+            throw new RuntimeException("이미 같은 날에 다른 메뉴의 식단표 투표를 진행하였습니다.");
+        }
+
+        //else 아닌지....?
         // 위의 두 경우가 모두 아니라면
         // 아직 투표 진행을 하지 않은 상태라면
         // 투표를 결과를 반영시켜줌
         MealVote mealvote = MealVote.createMealVote(
                 user,
                 mealPlanner,
-                request.getMenuNum()
+                request.getOrder()
         );
 
         // 투표 결과를 디비에 저장
@@ -114,5 +143,37 @@ public class MealService {
     /**
      * 13.4 식단표 조회
      */
+    // mealPlannerId 를 넘겨줘야 함
+    // 해당 user 의 투표 결과도!
+    public List<FindMealPlannerResponse> findMealPlannerList(int userId) {
+
+        System.out.println("MealService.findMealPlannerList");
+        // 스크랩하려는 유저 확인
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저가 없습니다."));
+
+        // user의 캠퍼스를 찾고 오늘 날짜로 mealPlanner를 찾음
+        List<MealPlanner> mealPlannerList = mealRepository.findByMealPlannerDateAndCampus(LocalDate.now(), user.getCampus());
+
+        List<FindMealPlannerResponse> result = new ArrayList<>();
+        for (MealPlanner mealPlanner : mealPlannerList) {
+            FindMealPlannerResponse build = FindMealPlannerResponse.builder()
+                    .mealPlannerId(mealPlanner.getId())
+//                    .campus(mealPlanner.getCampus())
+//                    .mealPlannerDate(mealPlanner.getMealPlannerDate())
+                    .mealOrder(mealPlanner.getMealOrder())
+                    .menu(mealPlanner.getMenu())
+                    .vote(mealPlanner.getVote())
+                    .build();
+
+            result.add(build);
+        }
+
+        return result;
+    }
+
+
+
+
 
 }
