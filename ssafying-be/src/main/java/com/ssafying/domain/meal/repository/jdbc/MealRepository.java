58bd4cfedc7e30ad4e632030fc9eaaf372c84356
Hgstudio <tndms0308@naver.com>
2024-02-15 14:12:55 +0900

    fix: MealRepository findByMealPlannerDateAndCampusAndMealOrder 메서드 삭제

--- ssafying-be/src/main/java/com/ssafying/domain/meal/repository/jdbc/MealRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/repository/jdbc/MealRepository.java
@@ -1,16 +1,14 @@
 package com.ssafying.domain.meal.repository.jdbc;
 
-import com.ssafying.domain.meal.entity.MealOrderStatus;
 import com.ssafying.domain.meal.entity.MealPlanner;
 import com.ssafying.domain.shuttle.entity.Campus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface MealRepository extends JpaRepository<MealPlanner, Integer> {
-    Optional<MealPlanner> findByMealPlannerDateAndCampusAndMealOrder(LocalDate mealPlannerDate, Campus campus, MealOrderStatus mealOrder);
+//    Optional<MealPlanner> findByMealPlannerDateAndCampusAndMealOrder(LocalDate mealPlannerDate, Campus campus, MealOrderStatus mealOrder);
 
-    List<MealPlanner> findByMealPlannerDateAndCampus(LocalDate now, Campus campeus);
+    List<MealPlanner> findByMealPlannerDateAndCampus(LocalDate now, Campus campus);
 }
