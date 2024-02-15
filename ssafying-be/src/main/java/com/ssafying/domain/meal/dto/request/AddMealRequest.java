0b6b424417be18fc507f067780f11763c9e8cd82
Hgstudio <tndms0308@naver.com>
2024-02-15 14:11:24 +0900

    fix: AddMealRequest mealOrder 칼럼 삭제

--- ssafying-be/src/main/java/com/ssafying/domain/meal/dto/request/AddMealRequest.java
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/dto/request/AddMealRequest.java
@@ -1,6 +1,5 @@
 package com.ssafying.domain.meal.dto.request;
 
-import com.ssafying.domain.meal.entity.MealOrderStatus;
 import com.ssafying.domain.shuttle.entity.CampusRegion;
 import lombok.*;
 
@@ -12,7 +11,7 @@ public class AddMealRequest {
 
     private CampusRegion campusRegion; // 캠퍼스 지역
 
-    private MealOrderStatus mealOrder; // 몇번째 메뉴인지
+//    private MealOrderStatus mealOrder; // 몇번째 메뉴인지
 
     private String menu; // 식단표 메뉴 (imgUrl)
 
