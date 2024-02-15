4e531ed57b970376c767d308f1b9570c1a1779fc
Hgstudio <tndms0308@naver.com>
2024-02-15 13:21:19 +0900

    fix: MealVoteRepository 안쓰는 sql 주석

--- ssafying-be/src/main/java/com/ssafying/domain/meal/repository/jdbc/MealVoteRepository.java
+++ ssafying-be/src/main/java/com/ssafying/domain/meal/repository/jdbc/MealVoteRepository.java
@@ -1,25 +1,21 @@
 package com.ssafying.domain.meal.repository.jdbc;
 
-import com.ssafying.domain.meal.entity.MealOrderStatus;
 import com.ssafying.domain.meal.entity.MealPlanner;
 import com.ssafying.domain.meal.entity.MealVote;
 import com.ssafying.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MealVoteRepository extends JpaRepository<MealVote, Integer> {
 
-    @Query("""
-            select count(*)
-            from MealVote mv
-            where mv.user = :user
-            and mv.mealPlanner = :mealPlanner
-            and mv.order = :order
-            """)
-    public int findByUserAndMealPlannerAndOrder(@Param("user") User user, @Param("mealPlanner") MealPlanner mealPlanner, @Param("order") MealOrderStatus order);
+//    @Query("""
+//            select count(*)
+//            from MealVote mv
+//            where mv.user = :user
+//            and mv.mealPlanner = :mealPlanner
+//            """)
+//    public int findByUserAndMealPlannerAndOrder(@Param("user") User user, @Param("mealPlanner") MealPlanner mealPlanner, @Param("order") MealOrderStatus order);
 
     Optional<MealVote> findByUserAndMealPlanner(User user, MealPlanner mealPlanner);
 }
